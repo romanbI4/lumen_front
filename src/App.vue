b9a5348e1b8117b54da098626bf13cce9feab59a
roman vdovenko <roman.vdovenko@idapgroup.com>
2022-12-10 16:36:07 +0200

    Refactored logic for menu with authorization. Added component for Logout.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="nav">
-    <!--ToDo need refactor-->
-    <router-link to="/login">Login</router-link>
-    |
-    <router-link to="/registration">Registration</router-link>
-    |
-    <router-link to="/companies">List Companies</router-link>
-    |
-    <router-link to="/companies/create">Create Company</router-link>
+    <router-link to="/login">Login </router-link>
+    <router-link to="/registration">Registration </router-link>
+    <router-link to="/companies">List Companies </router-link>
+    <router-link to="/companies/create">Create Company </router-link>
+    <router-link to="/logout">Logout </router-link>
   </div>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,69 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Login from '../components/LoginComponent.vue'
 import Registration from '../components/RegistationComponent.vue'
+import Logout from '../components/LogoutComponent.vue'
 import ListCompanies from '../components/companies/ListComponent.vue'
 import CreateCompany from '../components/companies/CreateComponent.vue'
-import EditCompany from "@/components/companies/EditComponent";
+import EditCompany from '../components/companies/EditComponent';
+
+const ifAuthenticated = () => {
+    return !!localStorage.getItem('token');
+};
 
 const routes = [
     {
         path: '/login',
         name: 'Login',
         component: Login,
-        meta: { guest: true },
+        meta: { requiresAuth: false },
+        beforeEnter: () => {
+            return !ifAuthenticated()
+        },
     },
     {
         path: '/registration',
         name: 'Registration',
         component: Registration,
-        meta: { guest: true },
+        meta: { requiresAuth: false },
+        beforeEnter: () => {
+            return !ifAuthenticated()
+        },
+    },
+    {
+        path: '/logout',
+        name: 'Logout',
+        component: Logout,
+        meta: { requiresAuth: true },
+        beforeEnter: () => {
+            return ifAuthenticated()
+        },
     },
     {
         path: '/companies/:id',
         name: 'EditCompany',
         component: EditCompany,
         meta: { requiresAuth: true },
+        beforeEnter: () => {
+            return ifAuthenticated()
+        },
     },
     {
         path: '/companies',
         component: ListCompanies,
         name: 'ListCompanies',
         meta: { requiresAuth: true },
+        beforeEnter: () => {
+            return ifAuthenticated()
+        },
     },
     {
         path: '/companies/create',
         component: CreateCompany,
         name: 'CreateCompany',
         meta: { requiresAuth: true },
+        beforeEnter: () => {
+            return ifAuthenticated()
+        },
     },
 ]
 
